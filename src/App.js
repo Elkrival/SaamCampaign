48bd2a3931f4896dd488a13a856952e3d5a5b958
Ivan Trujillo <krival1987@gmail.com>
2018-02-11 16:34:38 -0500

    completed header for saam campaign

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from './components/Header.component'
+
+class App extends Component {
+  render() {
+    return (
+     <Header />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.component.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/header.style.css'
+
+const Header = () =>{
+ return(
+  <header className="header">
+   <div className="chat-box">
+    <h1 className="heading-primary">
+     <span className="heading-primary-main">Day of Action Event </span>
+     <span className="heading-primary-sub">Sexual Awareness Month</span>
+    </h1>
+   <p class="paragraph-text">I’m reaching out to you, because the National Sexual Violence Resource Center has a wonderful initiative that we each as a community can take part of to further raise awareness where violence and sexual assault is concerned. For the Month of April there will be a lot of SAAM campaigns and events centered around this topic.</p>
+   </div>
+  </header>
+ )
+}
+
+export default Header
